a431b262dc0a395cf5421ca05caf5b1eab27eac9
Alan Lima <alan.silva1f@gmail.com>
2023-08-13 20:52:41 -0300

    postando dados

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 const menuDrop = document.querySelector(".menu--drop");
 const dropDownBox = document.querySelector(".drop--down--box");
 const logoAside = document.querySelector(".logo--aside");
-const header = document.querySelector(".header")
-const menuBtn = document.querySelector("#menu-btn");
+const header = document.querySelector(".header");
+const menuBtn = document.querySelector("#menu-btn > i");
 const boxAside = document.querySelector(".box-aside");
 
 menuDrop.addEventListener("mousemove", () => {
@@ -13,21 +13,21 @@ menuDrop.addEventListener("mouseout", () => {
   dropDownBox.classList.remove("active--menu");
 });
 
-// ======//====== //
 // logo--rigth //
-
 window.addEventListener("scroll", () => {
   const scrollLogo = window.scrollY;
 
-  if(scrollLogo > 50){
-    logoAside.classList.add("active")
-  }else{
-    logoAside.classList.remove("active");
-  }
+  scrollLogo > 50
+    ? logoAside.classList.add("active")
+    : logoAside.classList.remove("active");
+
+    menuBtn.classList.remove('fa-xmark');
+    boxAside.classList.remove("active--box--aside");
 });
 
-const activeMenuAside = () =>{
-  boxAside.classList.toggle("active--box--aside")
-}
+const activeMenuAside = () => {
+  menuBtn.classList.toggle('fa-xmark');
+  boxAside.classList.toggle("active--box--aside");
+};
 
 menuBtn.addEventListener("click", activeMenuAside);
