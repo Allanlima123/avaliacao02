6cedab74e8dcac86fb52b841298ca13dd10efb52
Alan Lima <alan.silva1f@gmail.com>
2023-08-13 00:08:24 -0300

    postando dados

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 const menuDrop = document.querySelector(".menu--drop");
 const dropDownBox = document.querySelector(".drop--down--box");
 const logoAside = document.querySelector(".logo--aside");
+const header = document.querySelector(".header")
+const menuBtn = document.querySelector("#menu-btn");
+const boxAside = document.querySelector(".box-aside");
 
 menuDrop.addEventListener("mousemove", () => {
   dropDownBox.classList.add("active--menu");
@@ -16,7 +19,15 @@ menuDrop.addEventListener("mouseout", () => {
 window.addEventListener("scroll", () => {
   const scrollLogo = window.scrollY;
 
-  scrollLogo > 50
-    ? logoAside.classList.add("active")
-    : logoAside.classList.remove("active");
+  if(scrollLogo > 50){
+    logoAside.classList.add("active")
+  }else{
+    logoAside.classList.remove("active");
+  }
 });
+
+const activeMenuAside = () =>{
+  boxAside.classList.toggle("active--box--aside")
+}
+
+menuBtn.addEventListener("click", activeMenuAside);
